55684e52b0db29446455106841ec32d46b8b60d4
adamd <adamdoyle630@gmail.com>
2024-02-20 21:52:57 -0500

    first api endpoints

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from static_files import StaticFileMiddleware
 from word_vectors import Word
 import word_vectors
 import requests
+from httpx import AsyncClient
 
 app = FastAPI()
 
@@ -11,19 +12,24 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.post("/api/get_word")
-def set_word(word: str) -> Word:
-    """Set Word and calculate dictionary of lookup-able words"""
-    try:
-        return word_vectors.get_word_info(word)
-    except Exception as e:
-        return HTTPException(status_code=502)
+@app.get("/api/get-word")
+async def fetch_word():
+    async with AsyncClient() as client:
+        response = await client.get("https://api.contexto.me/machado/en/giveup/520")
+        return response.json()
 
-url = "http://localhost:8000/api/get_word"
-data = {"word": "example"}
-headers = {"Content-Type": "application/json"}
+# @app.post("/api/get_word")
+# def set_word(word: str) -> Word:
+#     """Set Word and calculate dictionary of lookup-able words"""
+#     try:
+#         return word_vectors.get_word_info(word)
+#     except Exception as e:
+#         return HTTPException(status_code=502)
+
+# url = "http://localhost:8000/api/get_word"
+# data = {"word": "example"}
+# headers = {"Content-Type": "application/json"}
 
 # response = requests.put(url, json=data, headers=headers)
-
 # print(response.status_code)
 # print(response.json())
